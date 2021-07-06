7c4d047e549ac5a3ebd3c684ec4668fc325ec00a
Terrier Julien <sam@google.com>
2021-07-06 13:57:25 +0200

    python doc

---
+++ python/README.md
@@ -0,0 +1,1 @@
+# Dossier de documentation Python
---
+++ python/Samples/functions.py
@@ -0,0 +1,10 @@
+"""
+Simple function template
+"""
+def my_function(fname):
+  print(fname + " Refsnes")
+"""
+Simple lambda function template
+"""
+x = lambda a, b : a * b
+print(x(5, 6))
